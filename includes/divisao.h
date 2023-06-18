71d66a174e45f4c9197835a997d3f910c8231aa0
CarlosZ70 <131053974+CarlosZ70@users.noreply.github.com>
2023-06-18 16:04:05 -0300

    Create divisao.h

---
+++ includes/divisao.h
@@ -0,0 +1,15 @@
+#infed DIVISAO_
+#define DIVISAO_H
+//
+#include <stdio.h>
+#include <math.h>
+
+	float divisao(float val1, float val2){
+	return val1 / val2;
+	}
+	//				
+	float resto(float val1, float val2){ 
+	return fmod(val1,val2);
+	}
+			
+#endif // !DEBUG
